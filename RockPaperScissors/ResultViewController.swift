0af3d79d0e38119f521ffc644b092c5dd638fb32
Jason Lemrond <jason.lemrond@icloud.com>
2015-11-13 12:06:12 -0500

    Add methods to establish winner.

--- RockPaperScissors/ResultViewController.swift
+++ RockPaperScissors/ResultViewController.swift
@@ -12,6 +12,16 @@ import Foundation
 class ResultViewController: UIViewController {
 
     var userSelection: String!
+    var compSelection: String!
+    
+    override func viewWillAppear(animated: Bool) {
+        compSelection = generateOpponentResult()
+        print("Comp has selected \(compSelection)")
+        
+        
+        
+        establishWinner()   
+    }
     
     override func viewDidLoad() {
         
@@ -31,7 +41,6 @@ class ResultViewController: UIViewController {
         }
         
         if let result = result {
-            print(result)
             return result
         } else {
             return "Scissor"
@@ -39,5 +48,42 @@ class ResultViewController: UIViewController {
         
     }
 
+    //Convert RPS to numbers.
+    func convertToNumber(selection: String) -> Int {
+        
+        switch selection {
+            case "Rock": return 1
+            case "Paper": return 2
+            case "Scissor": return 3
+            default: return 0
+        }
+        
+    }
     
+    
+    //Establish Winner.
+    func establishWinner() {
+        
+        let user = convertToNumber(userSelection)
+        let comp = convertToNumber(compSelection)
+        
+        print(user)
+        print(comp)
+        
+        if user == comp {
+            print("it's a Tie")
+        } else if user == 1 {
+            if comp == 2 {
+                print("Comp Wins")
+            } else {
+                print("User Wins")
+            }
+        } else if user > comp {
+            print("User Wins")
+        } else {
+            print("Comp Wins")
+        }
+        
+        
+    }
 }
