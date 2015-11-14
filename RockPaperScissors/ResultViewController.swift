72ea31b03f782abc49187d2b16ad8f682f2a2fb9
Jason Lemrond <jason.lemrond@icloud.com>
2015-11-13 23:53:46 -0500

    Add play again button and result label

--- RockPaperScissors/ResultViewController.swift
+++ RockPaperScissors/ResultViewController.swift
@@ -38,29 +38,35 @@ class ResultViewController: UIViewController {
     var userSelection: RPS!
     var compSelection: RPS!
     
+    var playAgain: UIButton!
+    var result: UILabel!
+    
     override func viewWillAppear(animated: Bool) {
         compSelection = RPS.init()
         print("Comp has selected \(compSelection)")
         
-        
-        
         establishWinner(userSelection, comp: compSelection)
+        
+        let x = UIButton(frame: CGRect(x: 10, y: 10, width: 10, height: 10))
+        self.view.addSubview(x)
     }
     
     override func viewDidLoad() {
         
-    }
+        //Set up Play Again Button.
+        playAgain = UIButton(frame: CGRect(x: 125, y: 450, width: 125, height: 40))
+        self.view.addSubview(playAgain)
+        
+        playAgain.setTitle("Play Again", forState: .Normal)
+        playAgain.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        playAgain.addTarget(self, action: "playAgainSelected", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //Set up Result Label.
+        result = UILabel(frame: CGRect(x: 125, y: 250, width: 125, height: 40))
+        result.textColor = UIColor.blackColor()
+        result.textAlignment = .Center
+        self.view.addSubview(result)
 
-    //Convert RPS to numbers.
-    func convertToNumber(selection: String) -> Int {
-        
-        switch selection {
-            case "Rock": return 1
-            case "Paper": return 2
-            case "Scissor": return 3
-            default: return 0
-        }
-        
     }
     
     
@@ -71,17 +77,24 @@ class ResultViewController: UIViewController {
         print(comp.rawValue)
         
         if user == comp {
-            print("It's a tie")
+            result.text = "It's a Tie!"
         } else {
             switch (user, comp) {
             case (.Rock, .Scissor), (.Scissor, .Paper), (.Paper, .Rock):
-                print("You Win!")
+                result.text = "You Win!"
             case (.Scissor, .Rock), (.Paper, .Scissor), (.Rock, .Paper):
-                print("You Lose!")
+                result.text = "You Lose, Sucker!"
             default:
                 print("I think something went wrong... Oops")
             }
-        }   
+        }
         
+        print(result.text)
+
+    }
+    
+    //Method to bring up previous screen (Play Again)
+    func playAgainSelected() {
+        dismissViewControllerAnimated(true, completion: nil)
     }
 }
